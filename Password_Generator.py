fa8e51bac3d93962770fd797920855115a6701f0
SANTHOSH RAMISETTI <97424519+21A91A05I0@users.noreply.github.com>
2022-09-11 18:52:10 +0530

    s=input().split(",") a=[] k="" for i in s:     a=i.split(":")     name=a[0]     num=a[1]     m=0     for i in num:         if int(i)<=len(name):             if int(i)>m:                 m=int(i)     if(m==0):         k+='X'     else:         k+=name[m-1] print(k)

---
+++ Password_Generator.py
@@ -0,0 +1,17 @@
+s=input().split(",")
+a=[]
+k=""
+for i in s:
+    a=i.split(":")
+    name=a[0]
+    num=a[1]
+    m=0
+    for i in num:
+        if int(i)<=len(name):
+            if int(i)>m:
+                m=int(i)
+    if(m==0):
+        k+='X'
+    else:
+        k+=name[m-1]
+print(k)
